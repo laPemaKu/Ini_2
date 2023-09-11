775b69c242d072ca3b7b477ead79d2189f856ce7
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-09-11 10:35:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inicijalno_ponavljanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upiši te niz znakova.");
+            string niz = Console.ReadLine().ToString();
+            niz = niz.Replace(" ", "_");
+            Console.WriteLine(niz);
+            Console.ReadKey();
+            
+        }
+    }
+}
